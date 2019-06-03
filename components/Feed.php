8dc6d627cee481ee52fa19ee42db39cee0e5b79c
Sergey Sukhorukov <mr.sukhorukov.sergey@gmail.com>
2019-06-04 00:29:24 +0300

    Integration

--- components/Feed.php
+++ components/Feed.php
@@ -1,6 +1,10 @@
 <?php namespace Prismify\Facebook\Components;
 
 use Cms\Classes\ComponentBase;
+use Prismify\Facebook\Models\Settings;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 
 class Feed extends ComponentBase
 {
@@ -65,6 +69,41 @@ class Feed extends ComponentBase
 
     public function listPosts()
     {
-        return;
+        $fb = new Facebook([
+            'app_id'        => Settings::get('fb_app_id'),
+            'app_secret'    => Settings::get('fb_app_secret'),
+            'page_id'       => Settings::get('fb_page_id')
+        ]);
+
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $fb->get(
+                '/'.$this->pageId.'/feed?limit=25',
+                '{access-token}'
+            );
+        } catch(FacebookResponseException $e) {
+            return 'Graph returned an error: ' . $e->getMessage();
+        } catch(FacebookSDKException $e) {
+            return 'Facebook SDK returned an error: ' . $e->getMessage();
+        }
+
+        $posts = $response->getGraphNode()->asArray()['data'];
+
+        $num = 1;
+
+        foreach ($posts as &$post){
+
+            // load only a limited number of posts (maxItems)
+            if($this->property('postsPerPage') < $num++) break;
+
+            if (isset($post->message))
+                $post->short = substr ($post->message, 0, $this->property('postsMaxDesc')).'...';
+            else
+                $post->short = '';
+
+            array_push($result, $post);
+        }
+
+        return $result;
     }
 }
